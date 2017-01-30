da3e0aed24fb4c474a655aa76eee7e9f76974ea7
sdecleen <steven.decleene@dhl.com>
2017-01-30 11:25:30 +0100

    - SteveGorithm improvement 2

--- src/main/java/org/revenge/pizza/steve/Block.java
+++ src/main/java/org/revenge/pizza/steve/Block.java
@@ -23,7 +23,19 @@ class BlockComparator implements Comparator<Block> {
     public int compare(Block block1, Block block2) {
         int block1Sum = block1.height + block1.width;
         int block2Sum = block2.height + block2.width;
-        return (block1Sum < block2Sum) ? 1: (block1Sum == block2Sum) ? 0: -1;
+        if(block1Sum < block2Sum) {
+            return 1;
+        } else if(block1Sum == block2Sum) {
+            if(block1.width < block2.width) {
+                return 1;
+            } else if(block1.width == block2.width) {
+                return 0;
+            } else {
+                return -1;
+            }
+        } else {
+            return -1;
+        }
     }
 
 }
