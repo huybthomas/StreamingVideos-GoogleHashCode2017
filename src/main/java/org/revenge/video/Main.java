1a0b276a265d71440b756ec482331955ddf5b60e
sdecleeen <st.decleene@gmail.com>
2017-02-23 19:44:58 +0100

    /

--- src/main/java/org/revenge/video/Main.java
+++ src/main/java/org/revenge/video/Main.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Main {
 
 //    private static final String[] INPUT_FILES = {"kittens.in", "me_at_the_zoo.in", "trending_today.in", "videos_worth_spreading.in"};
-    private static final String[] INPUT_FILES = {"test.in"};
+    private static final String[] INPUT_FILES = {"me_at_the_zoo.in"};
 
     private static int numberOfVideos;
     private static int numberOfEndPoints;
