b28e3bac56293a99b57b16db887470a691540af3
sdecleeen <st.decleene@gmail.com>
2017-02-23 20:04:59 +0100

    /

---
+++ src/main/java/org/revenge/video/TupleComparator.java
@@ -0,0 +1,19 @@
+package org.revenge.video;
+
+import be.stivizu.q2016.util.Tuple;
+
+import java.util.Comparator;
+
+/**
+ * Created by sdecleen.
+ */
+public class TupleComparator implements Comparator<Tuple> {
+
+    @Override
+    public int compare(Tuple t1, Tuple t2) {
+        Double distance1 = (Double) t1.getRhs();
+        Double distance2 = (Double) t2.getRhs();
+        return distance1 < distance2 ? -1 : distance1.equals(distance2) ? 0 : 1;
+    }
+
+}
